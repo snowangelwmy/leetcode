79a2ab2ea2cfe1599fb15663edc6b27e3166e88f
AngelaWang <wang533@usc.edu>
2018-11-23 15:07:24 -0800

    add solution for Q7

---
+++ Q2.java
@@ -0,0 +1,26 @@
+/**
+ * Test cases:
+ * 1: Input: 123, Output: 321.
+ * 2: Input: -123, Output: -321
+ * 3: Input: 120, Output: 21
+ */
+
+import java.lang.Math;
+
+class Solution {
+    public int reverse(int x) {
+        long reversedX = 0L;
+
+        int base = 10;
+        while(x!=0){
+            reversedX = reversedX*base + x%base;
+            x = x/base;
+        }
+
+        if (reversedX > Math.pow(2, 31)-1 || reversedX < -1 * Math.pow(2, 31)) {
+            return 0;
+        }
+
+        return (int) reversedX;
+    }
+}
