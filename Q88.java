e21adf99fb69e62fa47e2957ccd12d6359faf778
AngelaWang <wang533@usc.edu>
2018-12-02 17:59:15 -0800

    add solution for Q88

---
+++ Q88.java
@@ -0,0 +1,34 @@
+/**
+ * Test cases:
+ * 1: Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3, Output: [1,2,2,3,5,6]
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int idx1 = m-1;
+        int idx2 = n-1;
+        int idx = m+n-1;
+        while(idx1>=0 && idx2>=0) {
+            if(nums1[idx1]>nums2[idx2]){
+                nums1[idx] = nums1[idx1];
+                idx1--;
+            } else { //nums1[idx1]<=nums2[idx2]
+                nums1[idx] = nums2[idx2];
+                idx2--;
+            }
+            idx--;
+        }
+        while(idx2>=0){
+            nums1[idx] = nums2[idx2];
+            idx2--;
+            idx--;
+        }
+    }
+}
