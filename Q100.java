460ea0db9b00563a03c817dc04eb7c4d9fc71a62
AngelaWang <wang533@usc.edu>
2018-12-03 23:51:56 -0800

    add solution for Q100

---
+++ Q100.java
@@ -0,0 +1,72 @@
+/**
+ * Test cases:
+ * 1:
+ * Input:     1         1
+ *           / \       / \
+ *          2   3     2   3
+ *
+ *         [1,2,3],   [1,2,3]
+ *
+ * Output: true
+ *
+ * 2:
+ * Input:     1         1
+ *           /           \
+ *          2             2
+ *
+ *         [1,2],     [1,null,2]
+ *
+ * Output: false
+ *
+ * 3:
+ * Input:     1         1
+ *           / \       / \
+ *          2   1     1   2
+ *
+ *         [1,2,1],   [1,1,2]
+ *
+ * Output: false
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        List<Integer> pVals = new ArrayList<>();
+        preOrderTraversal(p, pVals);
+        List<Integer> qVals = new ArrayList<>();
+        preOrderTraversal(q, qVals);
+        if(pVals.size() != qVals.size()){
+            return false;
+        }
+
+        for(int i=0; i<pVals.size(); i++){
+            if((pVals.get(i) == null && qVals.get(i) != null)
+                    || (pVals.get(i) != null && qVals.get(i) == null)
+                    || (pVals.get(i) != null && qVals.get(i) != null && !pVals.get(i).equals(qVals.get(i)))) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void preOrderTraversal(TreeNode root, List<Integer> vals) {
+        if(root==null) {
+            vals.add(null);
+            return;
+        }
+
+        vals.add(new Integer(root.val));
+        preOrderTraversal(root.left, vals);
+        preOrderTraversal(root.right, vals);
+    }
+}
