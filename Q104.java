0a09032b6347108ea472be9c0b6ead48ace18723
AngelaWang <wang533@usc.edu>
2018-12-05 22:18:48 -0800

    add solution for Q104

---
+++ Q104.java
@@ -0,0 +1,30 @@
+/**
+ * Test cases:
+ * 1: Input:
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * Output: 3
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+import java.lang.Math;
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+
+        return 1+Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+}
