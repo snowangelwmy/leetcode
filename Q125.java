5b34bbccc595a49fe2862491b5b5cfb73b206933
AngelaWang <wang533@usc.edu>
2018-12-14 14:16:37 -0800

    add solution for Q125

---
+++ Q125.java
@@ -0,0 +1,34 @@
+/**
+ * Test cases:
+ * 1: Input: "A man, a plan, a canal: Panama"
+ * Output: true
+ * 2: Input: "race a car"
+ * Output: false
+ */
+
+import java.lang.StringBuilder;
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s==null){
+            return false;
+        } else if(s.length()==0){
+            return true;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            if(Character.isLetterOrDigit(s.charAt(i))){
+                builder.append(Character.toUpperCase(s.charAt(i)));
+            }
+        }
+        String newS = builder.toString();
+        for(int i=0; i<newS.length()/2; i++){
+            if(newS.charAt(i)!=newS.charAt(newS.length()-1-i)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
