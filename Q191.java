995a890e6b0cc899a9a6ddd506551537e3f4262f
Angela Wang <mewang@expedia.com>
2018-12-17 12:38:06 -0800

    add solution for Q191

---
+++ Q191.java
@@ -0,0 +1,27 @@
+/**
+ * Test cases:
+ * 1: Input: 00000000000000000000000000001011 -> 11
+ * Output: 3
+ * Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+ * 2: Input: 00000000000000000000000010000000 -> 128
+ * Output: 1
+ * Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+ * 3: Input: 11111111111111111111111111111101 -> -3
+ * Output: 31
+ * Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+ */
+
+public class Solution {
+  // you need to treat n as an unsigned value
+  public int hammingWeight(int n) {
+    int bits = 0;
+    int mask = 1;
+    for(int i=0; i<32; i++){
+      if((n&mask)!=0){
+        bits++;
+      }
+      mask <<= 1;
+    }
+    return bits;
+  }
+}
