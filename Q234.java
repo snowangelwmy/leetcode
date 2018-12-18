fc60c2afb5af6e04e1c2ed88e9782a34cce94064
Angela Wang <mewang@expedia.com>
2018-12-18 13:53:52 -0800

    add solution for Q234

---
+++ Q234.java
@@ -0,0 +1,45 @@
+/**
+ * Test cases:
+ * 1: Input: 1->2
+ * Output: false
+ * 2: Input: 1->2->2->1
+ * Output: true
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+import java.util.Stack;
+
+class Solution {
+  public boolean isPalindrome(ListNode head) {
+    int length = 0;
+    ListNode pointer = head;
+    while(pointer!=null){
+      length++;
+      pointer = pointer.next;
+    }
+
+    int offset = (length+1)/2;
+    ListNode fastPointer = head;
+    int count = 0;
+    Stack<Integer> firstHalfNums = new Stack<>();
+    while(count<offset){
+      if(length%2==0||count!=offset-1){
+        firstHalfNums.push(fastPointer.val);
+      }
+      fastPointer = fastPointer.next;
+      count++;
+    }
+    while(fastPointer!=null){
+      if(fastPointer.val!=firstHalfNums.pop()){
+        return false;
+      }
+      fastPointer = fastPointer.next;
+    }
+    return true;
+  }
+}
