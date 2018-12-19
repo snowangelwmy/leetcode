761e74182f74cfd0689932b0b847b476a5cafb70
Angela Wang <mewang@expedia.com>
2018-12-19 15:22:00 -0800

    add solution for Q257

---
+++ Q257.java
@@ -0,0 +1,51 @@
+/**
+ * Test cases:
+ * 1: Input:
+ *    1
+ *  /   \
+ * 2     3
+ *  \
+ *   5
+ * Output: ["1->2->5", "1->3"]
+ * Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+  public List<String> binaryTreePaths(TreeNode root) {
+    List<String> paths = new ArrayList<>();
+    if(root==null){
+      return paths;
+    }
+
+    List<String> parents = new ArrayList<>();
+    depthFirstSearch(root, paths, parents);
+    return paths;
+  }
+
+  private void depthFirstSearch(TreeNode root, List<String> paths, List<String> parents) {
+    int index = parents.size();
+    parents.add(String.valueOf(root.val));
+    if(root.left==null&&root.right==null){ //root is a leaf node
+      paths.add(String.join("->", parents));
+    } else {
+      if(root.left!=null){
+        depthFirstSearch(root.left, paths, parents);
+      }
+      if(root.right!=null){
+        depthFirstSearch(root.right, paths, parents);
+      }
+    }
+    parents.remove(index);
+  }
+}
