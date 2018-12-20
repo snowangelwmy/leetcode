1d235c6a4bb3dee3a367b484475d837beb22a8ca
Angela Wang <mewang@expedia.com>
2018-12-19 16:15:30 -0800

    add solution for Q258

--- Q258.java
+++ Q258.java
@@ -4,10 +4,24 @@
  * Output: 2
  * Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2.
  *              Since 2 has only one digit, return it.
+ * 2: Input: 9
+ * Output: 9
  */
 
 class Solution {
+
+  //without any loop/recursion in O(1) runtime
   public int addDigits(int num) {
+    if(num<0){
+      return -1;
+    } else if(num==0){
+      return 0;
+    } else {
+      return (num%9==0)?9:(num%9);
+    }
+  }
+
+  public int addDigits1(int num) {
     if(num<0){
       return -1;
     }
