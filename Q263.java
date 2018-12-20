3bb5817772ed74ba451480cc9103a9be3640df59
Angela Wang <mewang@expedia.com>
2018-12-19 16:28:07 -0800

    add solution for Q263

---
+++ Q263.java
@@ -0,0 +1,37 @@
+/**
+ * Test cases:
+ * 1: Input: 6
+ * Output: true
+ * Explanation: 6 = 2 × 3
+ * 2: Input: 8
+ * Output: true
+ * Explanation: 8 = 2 × 2 × 2
+ * 3: Input: 14
+ * Output: false
+ * Explanation: 14 is not ugly since it includes another prime factor 7.
+ * 4: Input: 1
+ * Output: true
+ */
+
+class Solution {
+  public boolean isUgly(int num) {
+    if(num<=0){
+      return false;
+    }
+    //num>1
+    while(num%2==0){
+      num /= 2;
+    }
+    while(num%3==0){
+      num /= 3;
+    }
+    while(num%5==0){
+      num /= 5;
+    }
+    if(num==1){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
