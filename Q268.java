8fc53c0ec3e9aa21bb03c2ce7de1e899236a7406
Angela Wang <mewang@expedia.com>
2018-12-19 16:53:02 -0800

    add solution for Q268

---
+++ Q268.java
@@ -0,0 +1,22 @@
+/**
+ * Test cases:
+ * 1: Input: [3,0,1]
+ * Output: 2
+ * 2: Input: [9,6,4,2,3,5,7,0,1]
+ * Output: 8
+ */
+
+class Solution {
+  public int missingNumber(int[] nums) {
+    if(nums==null||nums.length==0){
+      return Integer.MIN_VALUE;
+    }
+    int n = nums.length;
+    int missing = n;
+    for(int i=0; i<n; i++){
+      missing ^= i;
+      missing ^= nums[i];
+    }
+    return missing;
+  }
+}
