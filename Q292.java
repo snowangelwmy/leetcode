fc8409cf6b942bfea85a2eb41ddf5b7a709d13fd
Angela Wang <mewang@expedia.com>
2018-12-20 16:35:56 -0800

    add solution for Q292

---
+++ Q292.java
@@ -0,0 +1,18 @@
+/**
+ * Test cases:
+ * 1: Input: 4
+ * Output: false
+ * Explanation: If there are 4 stones in the heap, then you will never win the game;
+ *              No matter 1, 2, or 3 stones you remove, the last stone will always be
+ *              removed by your friend.
+ */
+
+class Solution {
+  public boolean canWinNim(int n) {
+    if(n<=0){
+      System.out.println("Invalid parameter!!!");
+      return false;
+    }
+    return (n%4)!=0;
+  }
+}
