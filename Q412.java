ede12684ba9fba00f2cc1170a3b4f09fc9c90118
Angela Wang <mewang@expedia.com>
2018-12-27 15:50:51 -0800

    add solution for Q412

---
+++ Q412.java
@@ -0,0 +1,51 @@
+/**
+ * Test cases:
+ * 1: Input: 15
+ * Output:
+ * [
+ *     "1",
+ *     "2",
+ *     "Fizz",
+ *     "4",
+ *     "Buzz",
+ *     "Fizz",
+ *     "7",
+ *     "8",
+ *     "Fizz",
+ *     "Buzz",
+ *     "11",
+ *     "Fizz",
+ *     "13",
+ *     "14",
+ *     "FizzBuzz"
+ * ]
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+  public List<String> fizzBuzz(int n) {
+    List<String> nums = new ArrayList<>();
+    if(n<=0){
+      return nums;
+    }
+    //n>=1
+    for(int i=1; i<=n; i++){
+      String output = "";
+      if(i%3!=0&&i%5!=0){
+        output += String.valueOf(i);
+      } else {
+        if(i%3==0){
+          output += "Fizz";
+        }
+        if(i%5==0){
+          output += "Buzz";
+        }
+      }
+      nums.add(output);
+    }
+
+    return nums;
+  }
+}
