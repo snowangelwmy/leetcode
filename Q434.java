5ba85d2b7a4fb8898331c1341c0d8cabf05e49bd
Angela Wang <mewang@expedia.com>
2018-12-28 11:54:06 -0800

    add solution for Q434

---
+++ Q434.java
@@ -0,0 +1,32 @@
+/**
+ * Test cases:
+ * 1: Input: "Hello, my name is John"
+ * Output: 5
+ */
+
+class Solution {
+  public int countSegments(String s) {
+    int count = 0;
+    if(s==null||s.length()==0){
+      return count;
+    }
+    int lastStart = -1;
+    for(int i=0; i<s.length(); i++){
+      char c = s.charAt(i);
+      if(c==' '){
+        if(lastStart>=0){
+          count++;
+          lastStart = -1;
+        }
+      } else {
+        if(lastStart==-1){
+          lastStart = i;
+        }
+      }
+    }
+    if(lastStart>=0){
+      count++;
+    }
+    return count;
+  }
+}
