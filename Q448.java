3ec10b3e5f964a3f41ab2ca8bf59bbec60f665af
AngelaWang <wang533@usc.edu>
2019-01-03 22:58:51 -0800

    add solution for Q448

---
+++ Q448.java
@@ -0,0 +1,31 @@
+/**
+ * Test cases:
+ * 1: Input: [4,3,2,7,8,2,3,1]
+ * Output: [5,6]
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.Math;
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> numbers = new ArrayList<>();
+        if(nums==null||nums.length==0){
+            return numbers;
+        }
+        for(int i=0; i<nums.length; i++){
+            int index = Math.abs(nums[i])-1;
+            if(nums[index]>0){
+                nums[index] = -nums[index];
+            }
+        }
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]>0){
+                numbers.add(i+1);
+            }
+        }
+
+        return numbers;
+    }
+}
