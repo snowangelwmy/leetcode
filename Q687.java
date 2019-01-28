a17d78a41b87e142a3fec5fe26e8cb8551becab9
AngelaWang <wang533@usc.edu>
2019-01-28 02:39:15 -0800

    add solution for Q687

---
+++ Q687.java
@@ -0,0 +1,54 @@
+/**
+ * Test cases:
+ * 1: Input:
+ *               5
+ *              / \
+ *             4   5
+ *            / \   \
+ *           1   1   5
+ * Output: 2
+ * 2: Input:
+ *               1
+ *              / \
+ *             4   5
+ *            / \   \
+ *           4   4   5
+ * Output: 2
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public class Length {
+        private int length;
+    }
+
+    public int longestUnivaluePath(TreeNode root) {
+        Length lpLength = new Length();
+        longestUnivaluePath(root, lpLength);
+        return lpLength.length;
+    }
+
+    private int longestUnivaluePath(TreeNode root, Length lpLength) {
+        if(root==null) {
+            return 0;
+        }
+
+        int left = longestUnivaluePath(root.left, lpLength);
+        int right = longestUnivaluePath(root.right, lpLength);
+        int arrowLeft = 0, arrowRight = 0;
+        if(root.left!=null&&root.val==root.left.val) {
+            arrowLeft += left + 1;
+        }
+        if(root.right!=null&&root.val==root.right.val) {
+            arrowRight += right + 1;
+        }
+        lpLength.length = Math.max(lpLength.length, arrowLeft+arrowRight);
+        return Math.max(arrowLeft, arrowRight);
+    }
+}
