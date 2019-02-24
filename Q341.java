268070ac9d174814477bc62ce9c692d0ba996e53
AngelaWang <wang533@usc.edu>
2019-02-24 08:36:07 -0800

    add solution for Q341

---
+++ Q341.java
@@ -0,0 +1,69 @@
+/**
+ *
+ * Test cases:
+ * 1: Input: [[1,1],2,[1,1]]
+ * Output: [1,1,2,1,1]
+ * Explanation: By calling next repeatedly until hasNext returns false,
+ *              the order of elements returned by next should be: [1,1,2,1,1].
+ * 2: Input: [1,[4,[6]]]
+ * Output: [1,4,6]
+ * Explanation: By calling next repeatedly until hasNext returns false,
+ *              the order of elements returned by next should be: [1,4,6].
+ *
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+import java.util.Stack;
+
+public class NestedIterator implements Iterator<Integer> {
+    Stack<NestedInteger> nums = new Stack<>();
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        for(int i=nestedList.size()-1; i>=0; i--) {
+            nums.push(nestedList.get(i));
+        }
+    }
+
+    @Override
+    public Integer next() {
+        if(this.hasNext()) {
+            return nums.pop().getInteger();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        while(!nums.isEmpty()) {
+            NestedInteger cur = nums.peek();
+            if(cur.isInteger()) {
+                return true;
+            }
+
+            nums.pop();
+            for(int i=cur.getList().size()-1; i>=0; i--) {
+                nums.push(cur.getList().get(i));
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i = new NestedIterator(nestedList);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
