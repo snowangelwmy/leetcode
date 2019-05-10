0c03daf1726c3640b080c21d6b59c9e7fb461818
mewang <mewang@expedia.com>
2019-05-10 15:58:39 -0700

    update solution for Q95

--- Q95.java
+++ Q95.java
@@ -32,6 +32,13 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Q95 {
+  public class TreeNode {
+       int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
   public List<TreeNode> generateTrees(int n) {
     if(n<=0) {
       return new ArrayList<>();
