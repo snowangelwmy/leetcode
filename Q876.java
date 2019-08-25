afcce73ce26e235abe9ed65c4d298d51e8e4c137
mewang <mewang@expedia.com>
2019-08-24 23:39:33 -0700

    add solution for Q876

---
+++ Q876.java
@@ -0,0 +1,46 @@
+/**
+ * Test cases:
+ * 1: Input: [1,2,3,4,5]
+ * Output: Node 3 from this list (Serialization: [3,4,5])
+ * The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+ * Note that we returned a ListNode object ans, such that:
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+ * 2: Input: [1,2,3,4,5,6]
+ * Output: Node 4 from this list (Serialization: [4,5,6])
+ * Since the list has two middle nodes with values 3 and 4, we return the second one.
+ *
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Q876 {
+
+   public class ListNode {
+       int val;
+       ListNode next;
+       ListNode(int x) { val = x; }
+   }
+
+   public ListNode middleNode(ListNode head) {
+     if(head==null||head.next==null) {
+       return head;
+     }
+
+     ListNode fast = head;
+     ListNode slow = null;
+     while(fast!=null && fast.next!=null) {
+       if(slow==null) {
+         slow=head;
+       } else {
+         slow = slow.next;
+       }
+       fast = fast.next.next;
+     }
+
+     return slow.next;
+   }
+}
