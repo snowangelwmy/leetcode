6acb1353194fb85426adc69dcf96192424b3a78c
mewang <mewang@expedia.com>
2019-09-02 00:21:28 -0700

    add solution for Q105

---
+++ Q105.java
@@ -0,0 +1,71 @@
+/**
+ * Test cases:
+ * 1: Input:
+ * preorder = [3,9,20,15,7]
+ * inorder = [9,3,15,20,7]
+ * Output:
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Q105 {
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+  }
+
+  public TreeNode buildTree(int[] preorder, int[] inorder) {
+    if(preorder==null || inorder==null || preorder.length!=inorder.length || preorder.length==0) {
+      return null;
+    }
+
+    TreeNode root = new TreeNode(preorder[0]);
+    if(preorder.length==1) {
+      return root;
+    }
+
+    int rootIndex = -1;
+    for(int i=0; i<inorder.length; i++) {
+      if(inorder[i]==preorder[0]) {
+        rootIndex = i;
+      }
+    }
+    int[] leftInorder = new int[rootIndex];
+    int[] rightInorder = new int[inorder.length-rootIndex-1];
+    for(int i=0; i<inorder.length; i++) {
+      if(i<rootIndex) {
+        leftInorder[i] = inorder[i];
+      } else if(i>rootIndex) {
+        rightInorder[i-rootIndex-1] = inorder[i];
+      }
+    }
+    int[] leftPreorder = new int[rootIndex];
+    int[] rightPreorder = new int[preorder.length-rootIndex-1];
+    for(int i=1; i<preorder.length; i++) {
+      if(i<rootIndex+1) {
+        leftPreorder[i-1] = preorder[i];
+      } else if(i>rootIndex) {
+        rightPreorder[i-rootIndex-1] = preorder[i];
+      }
+    }
+    TreeNode left = buildTree(leftPreorder, leftInorder);
+    TreeNode right = buildTree(rightPreorder, rightInorder);
+    root.left = left;
+    root.right = right;
+    return root;
+  }
+}
