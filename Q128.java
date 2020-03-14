a3e9471c8142bc30ef2bde95b371f9b29c2b3e8f
mewang <mewang@expedia.com>
2020-03-14 15:44:36 -0700

    update solution for Q128

--- Q128.java
+++ Q128.java
@@ -3,7 +3,12 @@
  * 1: Input: [100, 4, 200, 1, 3, 2]
  * Output: 4
  * Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+ * 2: Input: [1,2,0,1]
+ * Output: 3
+ * Explanation: The longest consecutive elements sequence is [0, 1, 2]. Therefore its length is 3.
  */
+
+import java.util.Arrays;
 
 class Q128 {
     public int longestConsecutive(int[] nums) {
@@ -16,6 +21,7 @@ class Q128 {
         int longestLength = 1;
         int currentLength = 1;
         for(int i=1; i<nums.length; i++) {
+            //if(nums[i] == nums[i-1]), the consecutive sequence isn't considered broken.
             if (nums[i]!=nums[i-1]) {
                 if (nums[i]==nums[i-1]+1) {
                     currentLength++;
