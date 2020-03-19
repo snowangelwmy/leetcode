f34f8b0df271486782c193cbdfc0cfe001a8c329
mewang <mewang@expedia.com>
2020-03-19 03:01:01 -0700

    add solution for Q46

--- Q46.java
+++ Q46.java
@@ -22,6 +22,39 @@ class Q46 {
             return results;
         }
 
+        permuteHelper(nums, 0, results);
+        return results;
+    }
+
+    private void permuteHelper(int[] nums, int index, List<List<Integer>> results) {
+        if(index==nums.length) {
+            List<Integer> result = new ArrayList<>(nums.length);
+            for(int num: nums) {
+                result.add(num);
+            }
+            results.add(result);
+            return;
+        }
+
+        for(int i=index; i<nums.length; i++) {
+            swap(nums, index, i);
+            permuteHelper(nums, index+1, results);
+            swap(nums, index, i);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public List<List<Integer>> permute0(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        if(nums==null||nums.length==0) {
+            return results;
+        }
+
         permuteHelper(results, nums, new ArrayList<Integer>());
         return results;
     }
