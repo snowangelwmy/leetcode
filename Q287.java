2d419cf8b1456bb7a10def36a6816b9edb92de53
mewang <mewang@expedia.com>
2020-03-21 13:40:24 -0700

    add solution for Q287

--- Q287.java
+++ Q287.java
@@ -10,8 +10,38 @@ import java.util.Set;
 import java.util.HashSet;
 
 class Q287 {
-    //O(n) extra space
+
+    //O(1) space, O(nlog(n)) runtime complexity
+    //https://leetcode.com/problems/find-the-duplicate-number/discuss/443011/Accepted-Java-Binary-Search-Solution-(Meet-all-Requirements)
     public int findDuplicate(int[] nums) {
+        if(nums==null||nums.length==0) {
+            return Integer.MIN_VALUE;
+        }
+
+        int low = 0;
+        int high = nums.length-1;
+        while(low<high) {
+            int mid = (low+high)/2;
+
+            int count = 0;
+            for(int num : nums) {
+                if(num<=mid) {
+                    count++;
+                }
+            }
+
+            if(count>mid) {
+                high = mid;
+            } else {//count<=mid
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    //O(n) extra space
+    public int findDuplicate0(int[] nums) {
         if(nums==null||nums.length==0) {
             return Integer.MIN_VALUE;
         }
