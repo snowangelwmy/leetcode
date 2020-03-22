54841f0da28e52a3cc7ff0bbf5b984a72f9ecafc
mewang <mewang@expedia.com>
2020-03-22 12:28:02 -0700

    update solution for Q342

--- Q342.java
+++ Q342.java
@@ -7,7 +7,10 @@
  */
 
 class Q342 {
+  //https://leetcode.com/problems/power-of-four/discuss/80617/One-line-in-JAVA-without-loopsrecursion-which-is-extensible.
   public boolean isPowerOfFour(int num) {
+    //num>0 && ((num&(num-1))==0) are for power of 2.
+    //x^n - 1 = (x - 1)*(x^(n-1)+...+x+1); so x^n - 1 has a factor x - 1. here x = 4.
     return num>0 && ((num&(num-1))==0) && ((num-1)%3==0);
   }
 }
