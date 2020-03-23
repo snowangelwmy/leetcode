e530fdc9170d9669e38127c3ddb620cdae863458
mewang <mewang@expedia.com>
2020-03-23 11:06:46 -0700

    add solution for Q238

--- Q238.java
+++ Q238.java
@@ -5,7 +5,30 @@
  */
 
 class Q238 {
+
+    //with constant space complexity
     public int[] productExceptSelf(int[] nums) {
+        if(nums==null||nums.length<2) {
+            return null;
+        }
+
+        int[] result = new int[nums.length];
+        result[0] = 1;
+        for(int i=1; i<nums.length; i++) {
+            result[i] = result[i-1] * nums[i-1];
+        }
+
+        int right = 1;
+        for(int i=nums.length-2; i>=0; i--) {
+            right *= nums[i+1];
+            result[i] *= right;
+        }
+
+        return result;
+    }
+
+    //with extra space complexity
+    public int[] productExceptSelf0(int[] nums) {
         if(nums==null||nums.length<2) {
             return null;
         }
