512a5434dbffaccca09133fa950c97036b19bbf7
mewang <mewang@expedia.com>
2020-03-28 17:31:16 -0700

    update solution for Q1233

--- Q1233.java
+++ Q1233.java
@@ -1,3 +1,15 @@
+/**
+ * Test cases:
+ * 1: Input: folder = ["/a","/a/b","/c/d","/c/d/e","/c/f"]
+ * Output: ["/a","/c/d","/c/f"]
+ * Explanation: Folders "/a/b/" is a subfolder of "/a" and "/c/d/e" is inside of folder "/c/d" in our filesystem.
+ * 2: Input: folder = ["/a","/a/b/c","/a/b/d"]
+ * Output: ["/a"]
+ * Explanation: Folders "/a/b/c" and "/a/b/d/" will be removed because they are subfolders of "/a".
+ * 3: Input: folder = ["/a/b/c","/a/b/ca","/a/b/d"]
+ * Output: ["/a/b/c","/a/b/ca","/a/b/d"]
+ */
+
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
