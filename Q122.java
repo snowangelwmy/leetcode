bb058223f2a8aae65cc3393131c3db93ec414cfa
mewang <mewang@expedia.com>
2020-04-06 14:23:26 -0700

    update solution for Q122

--- Q122.java
+++ Q122.java
@@ -24,6 +24,22 @@ import java.util.HashMap;
 
 class Q122 {
 
+    //https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/solution/
+    public int maxProfit(int[] prices) {
+        if(prices==null||prices.length<=1) {
+            return 0;
+        }
+
+        int maxProfit = 0;
+        for(int i=1; i<prices.length; i++) {
+            if(prices[i]>prices[i-1]) {
+                maxProfit += prices[i] - prices[i-1];
+            }
+        }
+
+        return maxProfit;
+    }
+
     public int maxProfit2(int[] prices) {
         if(prices==null||prices.length<=1){
             return 0;
